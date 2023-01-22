ca2c73379d0e52f9f9e17e7382f9bc4f17c907aa
Muhammad <dev.muhammad1994@gmail.com>
2023-01-22 18:58:30 +0300

    Add style to Header cmp

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,38 +1,39 @@
 import React from 'react';
 import Container from '../ui/Container';
+import styles from './Header.module.scss';
 
 function Header() {
   return (
-    <nav className="navbar">
-      <Container>
-        <a href="/#" className="navbar__logo">
+    <nav className={styles['navbar']}>
+      <Container className={styles['navbar__container']}>
+        <a href="/#" className={styles['navbar__logo']}>
           <img src="logo.png" alt="MovieMosaic" />
         </a>
-        <ul className="navbar__nav">
-          <li className="navbar__item">
-            <a href="/#" className="navbar__link active">
+        <ul className={styles['navbar__nav']}>
+          <li className={styles['navbar__item']}>
+            <a href="/#" className={styles['navbar__link']}>
               Popular
             </a>
           </li>
-          <li className="navbar__item">
-            <a href="/#" className="navbar__link active">
+          <li className={styles['navbar__item']}>
+            <a href="/#" className={styles['navbar__link']}>
               Top Rated
             </a>
           </li>
-          <li className="navbar__item">
-            <a href="/#" className="navbar__link active">
+          <li className={styles['navbar__item']}>
+            <a href="/#" className={styles['navbar__link']}>
               Upcoming
             </a>
           </li>
-          <li className="navbar__item">
-            <a href="/#" className="navbar__link active">
+          <li className={styles['navbar__item']}>
+            <a href="/#" className={styles['navbar__link']}>
               Genre
             </a>
           </li>
-          <li className="navbar__item">
+          <li className={styles['navbar__item']}>
             <input
               type="search"
-              className="navbar__search"
+              className={styles['navbar__search']}
               placeholder="search movies..."
             />
           </li>
