3c14fc883073af27bac1fe9551d0865d0ee6ade0
Muhammad <dev.muhammad1994@gmail.com>
2023-01-23 20:10:07 +0300

    Create the Footer cmp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
     <>
       <Header />
       <h1>Hello React</h1>
+      <Footer />
     </>
   );
 }
