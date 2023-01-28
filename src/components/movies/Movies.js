133a39d9ba9ca300560c709b34457e85f7a5601b
Muhammad <dev.muhammad1994@gmail.com>
2023-01-28 22:38:22 +0300

    Create Moives cmp

---
+++ src/components/movies/Movies.js
@@ -0,0 +1,142 @@
+import React from 'react';
+
+function Movies() {
+  return (
+    <div>
+      <div>
+        <img
+          src="https://image.tmdb.org/t/p/original/1XSYOP0JjjyMz1irihvWywro82r.jpg"
+          alt="poster"
+        />
+        <h3>Violent Night</h3>
+        <div>
+          <p>8.7</p>
+          <p>2023</p>
+        </div>
+      </div>
+      <div>
+        <img
+          src="https://image.tmdb.org/t/p/original/1XSYOP0JjjyMz1irihvWywro82r.jpg"
+          alt="poster"
+        />
+        <h3>Violent Night</h3>
+        <div>
+          <p>8.7</p>
+          <p>2023</p>
+        </div>
+      </div>
+      <div>
+        <img
+          src="https://image.tmdb.org/t/p/original/1XSYOP0JjjyMz1irihvWywro82r.jpg"
+          alt="poster"
+        />
+        <h3>Violent Night</h3>
+        <div>
+          <p>8.7</p>
+          <p>2023</p>
+        </div>
+      </div>
+      <div>
+        <img
+          src="https://image.tmdb.org/t/p/original/1XSYOP0JjjyMz1irihvWywro82r.jpg"
+          alt="poster"
+        />
+        <h3>Violent Night</h3>
+        <div>
+          <p>8.7</p>
+          <p>2023</p>
+        </div>
+      </div>
+      <div>
+        <img
+          src="https://image.tmdb.org/t/p/original/1XSYOP0JjjyMz1irihvWywro82r.jpg"
+          alt="poster"
+        />
+        <h3>Violent Night</h3>
+        <div>
+          <p>8.7</p>
+          <p>2023</p>
+        </div>
+      </div>
+      <div>
+        <img
+          src="https://image.tmdb.org/t/p/original/1XSYOP0JjjyMz1irihvWywro82r.jpg"
+          alt="poster"
+        />
+        <h3>Violent Night</h3>
+        <div>
+          <p>8.7</p>
+          <p>2023</p>
+        </div>
+      </div>
+      <div>
+        <img
+          src="https://image.tmdb.org/t/p/original/1XSYOP0JjjyMz1irihvWywro82r.jpg"
+          alt="poster"
+        />
+        <h3>Violent Night</h3>
+        <div>
+          <p>8.7</p>
+          <p>2023</p>
+        </div>
+      </div>
+      <div>
+        <img
+          src="https://image.tmdb.org/t/p/original/1XSYOP0JjjyMz1irihvWywro82r.jpg"
+          alt="poster"
+        />
+        <h3>Violent Night</h3>
+        <div>
+          <p>8.7</p>
+          <p>2023</p>
+        </div>
+      </div>
+      <div>
+        <img
+          src="https://image.tmdb.org/t/p/original/1XSYOP0JjjyMz1irihvWywro82r.jpg"
+          alt="poster"
+        />
+        <h3>Violent Night</h3>
+        <div>
+          <p>8.7</p>
+          <p>2023</p>
+        </div>
+      </div>
+      <div>
+        <img
+          src="https://image.tmdb.org/t/p/original/1XSYOP0JjjyMz1irihvWywro82r.jpg"
+          alt="poster"
+        />
+        <h3>Violent Night</h3>
+        <div>
+          <p>8.7</p>
+          <p>2023</p>
+        </div>
+      </div>
+      <div>
+        <img
+          src="https://image.tmdb.org/t/p/original/1XSYOP0JjjyMz1irihvWywro82r.jpg"
+          alt="poster"
+        />
+        <h3>Violent Night</h3>
+        <div>
+          <p>8.7</p>
+          <p>2023</p>
+        </div>
+      </div>
+      <div>
+        <img
+          src="https://image.tmdb.org/t/p/original/1XSYOP0JjjyMz1irihvWywro82r.jpg"
+          alt="poster"
+        />
+        <h3>Violent Night</h3>
+        <div>
+          <p>8.7</p>
+          <p>2023</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Movies;
--- src/pages/TopMovies.js
+++ src/pages/TopMovies.js
@@ -1,11 +1,19 @@
 import React, { useEffect } from 'react';
+import Movies from '../components/movies/Movies';
+import Container from '../components/ui/Container';
 
 function TopMovies(props) {
   useEffect(() => {
     document.title = 'MovieMosaic || ' + props.title;
   }, [props.title]);
 
-  return <h1>{props.title}</h1>;
+  return (
+    <Container>
+      <h1>{props.title}</h1>
+      <p>Browse thousands of {props.title} through TMDB API</p>
+      <Movies />
+    </Container>
+  );
 }
 
 export default TopMovies;
