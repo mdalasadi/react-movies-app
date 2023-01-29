2edf8535f700c1f0e4fca0b0c67ac8e1faaee6c5
Muhammad <dev.muhammad1994@gmail.com>
2023-01-29 23:09:41 +0300

    Create Poster cmp

--- src/components/movies/Movies.js
+++ src/components/movies/Movies.js
@@ -1,140 +1,19 @@
 import React from 'react';
+import Poster from './Poster';
 
 function Movies() {
   return (
     <div>
-      <div>
-        <img
-          src="https://image.tmdb.org/t/p/original/1XSYOP0JjjyMz1irihvWywro82r.jpg"
-          alt="poster"
-        />
-        <h3>Violent Night</h3>
-        <div>
-          <p>8.7</p>
-          <p>2023</p>
-        </div>
-      </div>
-      <div>
-        <img
-          src="https://image.tmdb.org/t/p/original/1XSYOP0JjjyMz1irihvWywro82r.jpg"
-          alt="poster"
-        />
-        <h3>Violent Night</h3>
-        <div>
-          <p>8.7</p>
-          <p>2023</p>
-        </div>
-      </div>
-      <div>
-        <img
-          src="https://image.tmdb.org/t/p/original/1XSYOP0JjjyMz1irihvWywro82r.jpg"
-          alt="poster"
-        />
-        <h3>Violent Night</h3>
-        <div>
-          <p>8.7</p>
-          <p>2023</p>
-        </div>
-      </div>
-      <div>
-        <img
-          src="https://image.tmdb.org/t/p/original/1XSYOP0JjjyMz1irihvWywro82r.jpg"
-          alt="poster"
-        />
-        <h3>Violent Night</h3>
-        <div>
-          <p>8.7</p>
-          <p>2023</p>
-        </div>
-      </div>
-      <div>
-        <img
-          src="https://image.tmdb.org/t/p/original/1XSYOP0JjjyMz1irihvWywro82r.jpg"
-          alt="poster"
-        />
-        <h3>Violent Night</h3>
-        <div>
-          <p>8.7</p>
-          <p>2023</p>
-        </div>
-      </div>
-      <div>
-        <img
-          src="https://image.tmdb.org/t/p/original/1XSYOP0JjjyMz1irihvWywro82r.jpg"
-          alt="poster"
-        />
-        <h3>Violent Night</h3>
-        <div>
-          <p>8.7</p>
-          <p>2023</p>
-        </div>
-      </div>
-      <div>
-        <img
-          src="https://image.tmdb.org/t/p/original/1XSYOP0JjjyMz1irihvWywro82r.jpg"
-          alt="poster"
-        />
-        <h3>Violent Night</h3>
-        <div>
-          <p>8.7</p>
-          <p>2023</p>
-        </div>
-      </div>
-      <div>
-        <img
-          src="https://image.tmdb.org/t/p/original/1XSYOP0JjjyMz1irihvWywro82r.jpg"
-          alt="poster"
-        />
-        <h3>Violent Night</h3>
-        <div>
-          <p>8.7</p>
-          <p>2023</p>
-        </div>
-      </div>
-      <div>
-        <img
-          src="https://image.tmdb.org/t/p/original/1XSYOP0JjjyMz1irihvWywro82r.jpg"
-          alt="poster"
-        />
-        <h3>Violent Night</h3>
-        <div>
-          <p>8.7</p>
-          <p>2023</p>
-        </div>
-      </div>
-      <div>
-        <img
-          src="https://image.tmdb.org/t/p/original/1XSYOP0JjjyMz1irihvWywro82r.jpg"
-          alt="poster"
-        />
-        <h3>Violent Night</h3>
-        <div>
-          <p>8.7</p>
-          <p>2023</p>
-        </div>
-      </div>
-      <div>
-        <img
-          src="https://image.tmdb.org/t/p/original/1XSYOP0JjjyMz1irihvWywro82r.jpg"
-          alt="poster"
-        />
-        <h3>Violent Night</h3>
-        <div>
-          <p>8.7</p>
-          <p>2023</p>
-        </div>
-      </div>
-      <div>
-        <img
-          src="https://image.tmdb.org/t/p/original/1XSYOP0JjjyMz1irihvWywro82r.jpg"
-          alt="poster"
-        />
-        <h3>Violent Night</h3>
-        <div>
-          <p>8.7</p>
-          <p>2023</p>
-        </div>
-      </div>
+      <Poster />
+      <Poster />
+      <Poster />
+      <Poster />
+      <Poster />
+      <Poster />
+      <Poster />
+      <Poster />
+      <Poster />
+      <Poster />
     </div>
   );
 }
---
+++ src/components/movies/Poster.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Poster() {
+  return (
+    <div>
+      <img
+        src="https://image.tmdb.org/t/p/original/1XSYOP0JjjyMz1irihvWywro82r.jpg"
+        alt="poster"
+      />
+      <h3>Violent Night</h3>
+      <div>
+        <p>8.7</p>
+        <p>2023</p>
+      </div>
+    </div>
+  );
+}
+
+export default Poster;
