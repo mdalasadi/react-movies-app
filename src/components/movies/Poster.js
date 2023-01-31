c61bfdda1cc3affd0ce7b138ee90f11149b8f4a4
Muhammad <dev.muhammad1994@gmail.com>
2023-01-31 23:35:53 +0300

    Begin adding style to movies cmp

--- src/components/movies/Poster.js
+++ src/components/movies/Poster.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import styles from './Poster.module.scss';
 
 function Poster() {
   return (
-    <div>
+    <div className={styles['movies__poster']}>
       <img
         src="https://image.tmdb.org/t/p/original/1XSYOP0JjjyMz1irihvWywro82r.jpg"
         alt="poster"
+        className={styles['movies__image']}
       />
-      <h3>Violent Night</h3>
-      <div>
+      <h3 className={styles['movies__title']}>Violent Night</h3>
+      <div className={styles['movies__details']}>
         <p>8.7</p>
         <p>2023</p>
       </div>
