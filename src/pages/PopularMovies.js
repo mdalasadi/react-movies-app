628ef9f9fcbc142bac1602df69bf4573a46f1994
Muhammad <dev.muhammad1994@gmail.com>
2023-02-02 23:10:07 +0300

    Adjust style for the title of the pages

--- src/pages/PopularMovies.js
+++ src/pages/PopularMovies.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import Movies from '../components/movies/Movies';
 import Container from '../components/ui/Container';
+import styles from './Pages.module.scss';
 
 function PopularMovies(props) {
   useEffect(() => {
@@ -9,8 +10,10 @@ function PopularMovies(props) {
 
   return (
     <Container>
-      <h1>{props.title}</h1>
-      <p>Browse thousands of {props.title} through TMDB API</p>
+      <h1 className={styles['title']}>{props.title}</h1>
+      <p className={styles['lead']}>
+        Browse thousands of {props.title} through TMDB API
+      </p>
       <Movies />
     </Container>
   );
