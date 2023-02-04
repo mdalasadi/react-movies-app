4080db384186ebeef3f9fae9105605945afd0b5b
Muhammad <dev.muhammad1994@gmail.com>
2023-02-04 23:06:44 +0300

    Work on MovieDetails cmp

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -4,7 +4,30 @@ import Container from '../components/ui/Container';
 function MovieDetails() {
   return (
     <Container>
-      <h1>Movie Details</h1>
+      <section className="movie">
+        <img
+          src="https://image.tmdb.org/t/p/original/1XSYOP0JjjyMz1irihvWywro82r.jpg"
+          alt="movie poster"
+          className="movie__image"
+        />
+        <div className="movie__details">
+          <h2>Violent Night</h2>
+          <p>
+            Runtime: <span>1 hr & 47 min</span>
+          </p>
+          <p>
+            Genres: <span>Romance, Drama</span>
+          </p>
+          <p>
+            Description:
+            <span>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe
+              natus molestias perferendis incidunt est ad?
+            </span>
+          </p>
+          <p><a href="https://youtube.com" target="_blank" rel="noreferrer">View Trailer</a></p>
+        </div>
+      </section>
     </Container>
   );
 }
