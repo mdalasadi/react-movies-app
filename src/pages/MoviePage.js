27200a721cbe6cfb9442816c9109ced2bf54e30e
Muhammad <dev.muhammad1994@gmail.com>
2023-02-13 20:56:55 +0300

    Make movie name appear in the title of the tab

--- src/pages/MoviePage.js
+++ src/pages/MoviePage.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Container from '../components/ui/Container';
 import MovieDetails from '../components/movies/MovieDetails';
 
 function MoviePage() {
+  useEffect(() => {
+    document.title = 'MovieMosaic || Violent Night';
+  }, []);
+
   return (
     <Container>
       <MovieDetails />
