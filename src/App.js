ea645f01d71cd86cd96c28c005fa92b5cc33955a
Muhammad <dev.muhammad1994@gmail.com>
2023-02-26 21:01:05 +0300

    Fetch movies that the user searched for

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
--- src/pages/SearchResultsPage.js
+++ src/pages/SearchResultsPage.js
@@ -1,20 +1,42 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Movies from '../components/movies/Movies';
 import Container from '../components/ui/Container';
 import styles from './Pages.module.scss';
 import { useParams } from 'react-router-dom';
 
 function PopularMovies() {
-  const { name } = useParams();
+  const [searchResults, setSearchResults] = useState([]);
+  const { '*': searchQuery } = useParams();
 
   useEffect(() => {
     document.title = 'MovieMosaic || Popular Movies';
   }, []);
 
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=db32cb7fb5b3f64b6035a85dbfcd086d&language=en-US&query=${searchQuery}&page=1`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+
+        setSearchResults(
+          data.results.map((movie) => ({
+            id: movie.id,
+            image: movie.poster_path,
+            title: movie.title,
+            rate: movie.vote_average,
+            release_date: new Date(movie.release_date),
+          }))
+        );
+      })
+      .catch((error) => console.error(error));
+  }, [searchQuery]);
+
   return (
     <Container>
-      <h1 className={styles['title']}>You're Search Results:</h1>
-      <Movies movies={[]} />
+      <h1 className={styles['title']}>You're Searched For:</h1>
+      <Movies movies={searchResults} />
     </Container>
   );
 }
