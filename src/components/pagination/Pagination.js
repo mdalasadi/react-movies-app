d1e72ef292177e5b105a4103991eb8ce701113f2
Muhammad <dev.muhammad1994@gmail.com>
2023-03-05 23:46:30 +0300

    Working on pagination style

--- src/components/pagination/Pagination.js
+++ src/components/pagination/Pagination.js
@@ -4,13 +4,13 @@ import styles from './Pagination.module.scss';
 function Pagination() {
   return (
     <nav className={styles['pagination']}>
-      <button className={styles['pagination__prev disabled']}>Previous</button>
+      <button className={`${styles['pagination__button']} ${styles['pagination__button--prev']} ${styles['disabled']}`}>Previous</button>
       <ul className={styles['pagination__pages']}>
-        <li className={styles['pagination__page active']}>1</li>
+        <li className={styles['pagination__page']}>1</li>
         <li className={styles['pagination__page']}>2</li>
         <li className={styles['pagination__page']}>3</li>
       </ul>
-      <button className={styles['pagination__next']}>Next</button>
+      <button className={`${styles['pagination__button']} ${styles['pagination__button--next']}`}>Next</button>
     </nav>
   );
 }
