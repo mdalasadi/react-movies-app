3095b30400662389c332da19f73ccaa58c5515e1
Muhammad <dev.muhammad1994@gmail.com>
2023-03-06 20:43:40 +0300

    Complete pagination style

--- src/components/pagination/Pagination.js
+++ src/components/pagination/Pagination.js
@@ -4,13 +4,13 @@ import styles from './Pagination.module.scss';
 function Pagination() {
   return (
     <nav className={styles['pagination']}>
-      <button className={`${styles['pagination__button']} ${styles['pagination__button--prev']} ${styles['disabled']}`}>Previous</button>
+      <button type='button' className={`${styles['pagination__button']} ${styles['pagination__button--prev']} ${styles['disabled'] ?? ''}`.trim()}>Previous</button>
       <ul className={styles['pagination__pages']}>
-        <li className={styles['pagination__page']}>1</li>
+        <li className={`${styles['pagination__page']} ${styles['active'] ?? ''}`.trim()}>1</li>
         <li className={styles['pagination__page']}>2</li>
         <li className={styles['pagination__page']}>3</li>
       </ul>
-      <button className={`${styles['pagination__button']} ${styles['pagination__button--next']}`}>Next</button>
+      <button type='button' className={`${styles['pagination__button']} ${styles['pagination__button--next']}`}>Next</button>
     </nav>
   );
 }
